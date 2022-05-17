2a9399d92cd45033147074e434ef2c90b6e2a885
MelekElloumi <62803294+MelekElloumi@users.noreply.github.com>
2022-05-17 23:35:55 +0100

    adding Flask support: home page

--- app4test.py
+++ app4test.py
@@ -1,6 +1,36 @@
+from flask import Flask, render_template, flash, redirect, url_for, session, request, logging, jsonify
+import sqlite3
+from flask_mysqldb import MySQL
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
+from functools import wraps
+
 from loginModule import loginMenu
 from productModule import productMenu
-print("-------App4Test-------")
-username,password=loginMenu()
-productMenu()
-print("App4Test done, goodbye ^_^")
+from databaseInit import create_db
+
+def create_app(name):
+    app = Flask(name)
+    db_connection = sqlite3.connect('database.db', check_same_thread=False)
+    app.config.from_mapping(
+        DATABASE_CON=db_connection
+    )
+
+    # Index
+    @app.route('/')
+    def index():
+        return render_template('home.html')
+
+    return app
+
+
+# print("-------App4Test-------")
+# username,password=loginMenu()
+# productMenu()
+# print("App4Test done, goodbye ^_^")
+
+
+if __name__ == '__main__':
+    create_db('database.db')
+    app = create_app(__name__)
+    app.run()
--- databaseInit.py
+++ databaseInit.py
@@ -1,14 +1,22 @@
 import sqlite3
+import os
 
-connection = sqlite3.connect('database.db')
-connection.execute(
-    "CREATE TABLE User (ID INTEGER PRIMARY KEY, USERNAME TEXT NOT NULL, PASSWORD TEXT NOT NULL);")
-connection.execute(
-    "INSERT INTO User (USERNAME,PASSWORD) VALUES ('admin','admin'),('root','root'),('melek','elloumi');")
-connection.execute(
-    "CREATE TABLE Product (ID INTEGER PRIMARY KEY, NAME TEXT NOT NULL, PRICE DOUBLE NOT NULL, QUANTITY INT NOT NULL);")
-connection.execute(
-    "INSERT INTO Product (NAME,PRICE,QUANTITY) VALUES ('CD',2.0,5),('DVD',3.5,1),('Flash',10.0,2),('Pen',1.0,0);")
-connection.commit()
-print("Done")
-connection.close()
+def create_db(database_filename):
+    connection = sqlite3.connect(database_filename)
+    cur = connection.cursor()
+    cur.execute("DROP TABLE IF EXISTS User")
+    cur.execute("DROP TABLE IF EXISTS Product")
+    cur.execute(
+        "CREATE TABLE User (ID INTEGER PRIMARY KEY, USERNAME TEXT NOT NULL, PASSWORD TEXT NOT NULL);")
+    cur.execute(
+        "INSERT INTO User (USERNAME,PASSWORD) VALUES ('admin','admin'),('root','root'),('melek','elloumi');")
+    cur.execute(
+        "CREATE TABLE Product (ID INTEGER PRIMARY KEY, NAME TEXT NOT NULL, PRICE DOUBLE NOT NULL, QUANTITY INT NOT NULL);")
+    cur.execute(
+        "INSERT INTO Product (NAME,PRICE,QUANTITY) VALUES ('CD',2.0,5),('DVD',3.5,1),('Flash',10.0,2),('Pen',1.0,0);")
+    connection.commit()
+    print("Done")
+    connection.close()
+
+if __name__ == '__main__':
+    create_db('database.db')
