9cc3080be9952e0e7858a7c33cbd1aaf20dd3a57
MelekElloumi <62803294+MelekElloumi@users.noreply.github.com>
2022-05-18 00:33:27 +0100

    adding Flask support: authentication

--- app4test.py
+++ app4test.py
@@ -5,9 +5,10 @@ from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
 from functools import wraps
 
-from loginModule import loginMenu
+from loginModule import verifyUser,addUser
 from productModule import productMenu
 from databaseInit import create_db
+
 
 def create_app(name):
     app = Flask(name)
@@ -21,6 +22,67 @@ def create_app(name):
     def index():
         return render_template('home.html')
 
+    # Register Form Class
+    class RegisterForm(Form):
+        username = StringField('Username', [validators.Length(min=4, max=25)])
+        password = PasswordField('Password', [
+            validators.DataRequired(),
+            validators.EqualTo('confirm', message='Passwords do not match')
+        ])
+        confirm = PasswordField('Confirm Password')
+
+    # User Register
+    @app.route('/register', methods=['GET', 'POST'])
+    def register():
+        form = RegisterForm(request.form)
+        if request.method == 'POST' and form.validate():
+            username = form.username.data
+            password = str(form.password.data)
+
+            addUser(username,password)
+
+            flash('Registered successfully', 'success')
+
+            return redirect(url_for('login'))
+        return render_template('register.html', form=form)
+
+    # User login
+    @app.route('/login', methods=['GET', 'POST'])
+    def login():
+        if request.method == 'POST':
+            # Get Form Fields
+            username = request.form['username']
+            password = request.form['password']
+
+            result,error=verifyUser(username,password)
+
+            if result:
+                session['logged_in'] = True
+                session['username'] = username
+
+                flash('Logged in successfully', 'success')
+                return redirect(url_for('productapp'))
+            else:
+                return render_template('login.html', error=error)
+        return render_template('login.html')
+
+    def is_logged_in(f):
+        @wraps(f)
+        def wrap(*args, **kwargs):
+            if 'logged_in' in session:
+                return f(*args, **kwargs)
+            else:
+                flash('Unauthorized, Please login', 'danger')
+                return redirect(url_for('login'))
+
+        return wrap
+
+    # Dashboard
+    @app.route('/productapp')
+    @is_logged_in
+    def productapp():
+        return render_template('productapp.html')
+
     return app
 
 
@@ -33,4 +95,5 @@ def create_app(name):
 if __name__ == '__main__':
     create_db('database.db')
     app = create_app(__name__)
+    app.secret_key = 'secret123'
     app.run()
--- loginModule.py
+++ loginModule.py
@@ -1,19 +1,25 @@
 import sqlite3
 
-def verifyUser(username,password):
-    connection = sqlite3.connect('database.db')
-    data=connection.execute("SELECT PASSWORD FROM USER WHERE USERNAME=?;", (username,)).fetchone()
-
-    if(data==None):
-        print("User not registered in database")
-        connection.close()
-        return False
-    if(data[0]!=password):
-        print("Wrong password")
-        connection.close()
-        return False
+def addUser(username,password):
+    connection = sqlite3.connect('database.db', check_same_thread=False)
+    cur = connection.cursor()
+    cur.execute("INSERT INTO User(USERNAME,PASSWORD) VALUES(?, ?)", (username, password))
+    connection.commit()
     connection.close()
-    return True
+
+def verifyUser(username,password):
+    connection = sqlite3.connect('database.db', check_same_thread=False)
+    cur = connection.cursor()
+    data=cur.execute("SELECT PASSWORD FROM USER WHERE USERNAME=?;", (username,)).fetchone()
+    connection.commit()
+    connection.close()
+    if(data==None):
+        error="User not registered in database"
+        return False,error
+    if(data[0]!=password):
+        error="Wrong password"
+        return False,error
+    return True,""
 
 def loginMenu():
     loggedin=False
