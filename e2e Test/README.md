a963eea1b95e976bde1838864b07482cff5817d7
MelekElloumi <62803294+MelekElloumi@users.noreply.github.com>
2022-05-19 15:56:35 +0100

    finished e2e tests

--- e2e Test/README.md
+++ e2e Test/README.md
@@ -1,24 +1,16 @@
 # End 2 end Testing 
 
-- I wrote 17 test cases for 10 url routes of App4Test, from the Flask app in app4test.py.
-- With the 17 unit tests, it becomes 34 tests in total.
-- I used one fixture of session scope to create a test database for all the tests.
-- I used one fixture of module scope to create the app and get its context.
-- Each test contains 3 steps: 
-    1. Given
-    2. When
-    3. Then
-- I mainly tested the status code, title and alert messages of the directed response page.
+- I wrote 10 e2e test cases for App4Test.
+- 9 test cases cover normal execution and 1 test case covers a failed login.
+- I used selenium to navigate through the web pages
+- A total of 13 screenshots are saved during tests. (See them in e2e Test/Screenshots)
+- I used class methods setUpClass and tearDownClass to initialise and revert the testing environment.
+- I mainly tested the alert messages and the changes in the main table at productapp.html.
+- A temporary user and product were created during testing.
 
 ### Test Execution:
+![2014-10-22 11_35_09](https://j.gifs.com/NOjQ16.gif)
+![Imgur](https://i.imgur.com/gJJkaRf.png)
 
-![Imgur](https://i.imgur.com/YNp3wzk.png)
-
-- The 37 tests have passed.
-
-### Test Coverage:
-
-![Imgur](https://i.imgur.com/qhMntzB.png)
-
-- The test coverage is 99%.
-- The uncovered code is the main function of app4test.py that isn't called when creating the app.
+- The 10 tests have passed.
+- The total duration is 13.17 seconds.
--- e2e Test/test_e2e_app4test.py
+++ e2e Test/test_e2e_app4test.py
@@ -1,23 +1,33 @@
-import time
-from unittest import TestCase
+from unittest import TestCase,TestSuite,TextTestRunner
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import sqlite3
+import time
 
 #to run the test:
-#coverage run -m unittest "e2e Test\test_e2e_app4test.py"
+#coverage run python -m unittest "e2e Test\test_e2e_app4test.py"
+
+def deleteUser():
+    database_filename = 'database.db'
+    connection = sqlite3.connect(database_filename)
+    connection.execute(
+        "DELETE FROM USER WHERE USERNAME LIKE ?;", ("selenium",))
+    connection.commit()
+    connection.close()
 
 class TestApp4Test(TestCase):
 
     @classmethod
-    def setUp(inst):
+    def setUpClass(inst):
+        inst.start = time.time()
         inst.driver = webdriver.Chrome('chromedriver.exe')
         #inst.driver.implicitly_wait(1)
         print("Visiting home page")
         inst.driver.maximize_window()
         inst.driver.get('http://localhost:5000/')
-        inst.driver.save_screenshot('./e2e Test/Screenshots/homepage.png')
+        inst.driver.save_screenshot('./e2e Test/Screenshots/01_homepage.png')
 
-    def test_register(self):
+    def test_01_register(self):
         print("Visiting register page")
         register_button = self.driver.find_element(by=By.ID, value='Register')
         register_button.click()
@@ -29,29 +39,220 @@ class TestApp4Test(TestCase):
         password_field.send_keys('driver')
         confirm_field = self.driver.find_element(by=By.ID, value='confirm')
         confirm_field.send_keys('driver')
-        self.driver.save_screenshot('./e2e Test/Screenshots/registerpage.png')
+        self.driver.save_screenshot('./e2e Test/Screenshots/02_registerpage.png')
 
         print("Submitting register form")
         register_button = self.driver.find_element(by=By.ID, value='Submit')
         register_button.click()
 
+        print("Redirecting to login page")
         message = self.driver.find_element(by=By.ID, value='message0').text
         expected_message = "Registered successfully"
+
+        self.assertIn(expected_message, message)
+
+    def test_02_login_success(self):
+        print("Filling login form")
+        username_field = self.driver.find_element(by=By.NAME, value='username')
+        username_field.send_keys('selenium')
+        password_field = self.driver.find_element(by=By.NAME, value='password')
+        password_field.send_keys('driver')
+        self.driver.save_screenshot('./e2e Test/Screenshots/03_loginpage.png')
+
+        print("Submitting login form")
+        register_button = self.driver.find_element(by=By.ID, value='Submit')
+        register_button.click()
+
+        print("Redirecting to productapp page")
+        message = self.driver.find_element(by=By.ID, value='message0').text
+        expected_message = "Logged in successfully"
+
+        self.assertIn(expected_message, message)
+
+    def test_03_productapp(self):
+        print("Checking productapp page elements")
+        self.driver.save_screenshot('./e2e Test/Screenshots/04_productapppage_beforeadd.png')
+        table_id = self.driver.find_element(by=By.ID, value='maintable')
+        rows = table_id.find_elements(By.TAG_NAME, "tr")
+        col = rows[1].find_elements(By.TAG_NAME, "td")
+        cd_data={
+            "id":col[0].text,
+            "name": col[1].text,
+            "price": col[2].text,
+            "quantity": col[3].text
+        }
+        expected_cd_data = {
+            "id": "1",
+            "name": "CD",
+            "price": "2.0",
+            "quantity": "5"
+        }
+
+        self.assertEqual(expected_cd_data,cd_data)
+
+    def test_04_addproduct(self):
+        print("Visiting add product page")
+        addproduct_button = self.driver.find_element(by=By.ID, value='addproduct')
+        addproduct_button.click()
+
+        print("Filling add product form")
+        name_field = self.driver.find_element(by=By.ID, value='name')
+        name_field.send_keys('server')
+        price_field = self.driver.find_element(by=By.ID, value='price')
+        price_field.send_keys('42.0')
+        quantity_field = self.driver.find_element(by=By.ID, value='quantity')
+        quantity_field.send_keys('2')
+        self.driver.save_screenshot('./e2e Test/Screenshots/05_addproductpage.png')
+
+        print("Submitting add product form")
+        addproduct_button = self.driver.find_element(by=By.ID, value='Submit')
+        addproduct_button.click()
+
+        print("Redirecting to productapp page")
+        message = self.driver.find_element(by=By.ID, value='message0').text
+        expected_message = "Product Created"
+        self.driver.save_screenshot('./e2e Test/Screenshots/06_productapppage_afteradd.png')
+
+        table_id = self.driver.find_element(by=By.ID, value='maintable')
+        rows = table_id.find_elements(By.TAG_NAME, "tr")
+        col = rows[5].find_elements(By.TAG_NAME, "td")
+        server_data = {
+            "id": col[0].text,
+            "name": col[1].text,
+            "price": col[2].text,
+            "quantity": col[3].text
+        }
+        expected_server_data = {
+            "id": "5",
+            "name": "server",
+            "price": "42.0",
+            "quantity": "2"
+        }
+
+        self.assertIn(expected_message, message)
+        self.assertEqual(expected_server_data, server_data)
+
+    def test_05_editproduct(self):
+        print("Visiting edit page for the product 'server'")
+        editproduct_button = self.driver.find_element(by=By.ID, value='editproduct5')
+        editproduct_button.click()
+
+        print("Filling edit product form")
+        price_field = self.driver.find_element(by=By.ID, value='price')
+        price_field.clear()
+        price_field.send_keys('20.0')
+        self.driver.save_screenshot('./e2e Test/Screenshots/07_editproductpage.png')
+
+        print("Submitting add product form")
+        editproduct_button = self.driver.find_element(by=By.ID, value='Submit')
+        editproduct_button.click()
+
+        print("Redirecting to productapp page")
+        message = self.driver.find_element(by=By.ID, value='message0').text
+        expected_message = "Product updated successfully"
+        self.driver.save_screenshot('./e2e Test/Screenshots/08_productapppage_afteredit.png')
+
+        table_id = self.driver.find_element(by=By.ID, value='maintable')
+        rows = table_id.find_elements(By.TAG_NAME, "tr")
+        col = rows[5].find_elements(By.TAG_NAME, "td")
+        server_price = col[2].text
+        expected_server_price = "20.0"
+
+        self.assertIn(expected_message, message)
+        self.assertEqual(expected_server_price, server_price)
+
+    def test_06_buyproduct(self):
+        print("Buying the product 'server'")
+        buyproduct_button = self.driver.find_element(by=By.ID, value='buyproduct5')
+        buyproduct_button.click()
+
+        print("Redirecting to productapp page")
+        message = self.driver.find_element(by=By.ID, value='message0').text
+        expected_message = "Product bought successfully"
+        self.driver.save_screenshot('./e2e Test/Screenshots/09_productapppage_afterbuy.png')
+
+        table_id = self.driver.find_element(by=By.ID, value='maintable')
+        rows = table_id.find_elements(By.TAG_NAME, "tr")
+        col = rows[5].find_elements(By.TAG_NAME, "td")
+        server_price = col[3].text
+        expected_server_price = "1"
+
+        self.assertIn(expected_message, message)
+        self.assertEqual(expected_server_price, server_price)
+
+    def test_07_deleteproduct(self):
+        print("Deleting the product 'server'")
+        deleteproduct_button = self.driver.find_element(by=By.ID, value='deleteproduct5')
+        deleteproduct_button.click()
+
+        print("Redirecting to productapp page")
+        message = self.driver.find_element(by=By.ID, value='message0').text
+        expected_message = "Product Deleted"
+        self.driver.save_screenshot('./e2e Test/Screenshots/10_productapppage_afterdelete.png')
+
+        table_id = self.driver.find_element(by=By.ID, value='maintable')
+        rows = table_id.find_elements(By.TAG_NAME, "tr")
+        rows_number=len(rows)
+        expected_rows_number = 5
+
+        self.assertIn(expected_message, message)
+        self.assertEqual(expected_rows_number, rows_number)
+
+    def test_08_averageprice(self):
+        print("Checking the average price")
+        averageprice_button = self.driver.find_element(by=By.ID, value='averageproduct')
+        averageprice_button.click()
+
+        print("Redirecting to productapp page")
+        self.driver.save_screenshot('./e2e Test/Screenshots/11_productapppage_afteraverage.png')
+
+        average_price = self.driver.find_element(by=By.ID, value='average').text
+        expected_average_price = "Average: 4.125"
+
+        self.assertEqual(expected_average_price, average_price)
+
+    def test_09_logout(self):
+        print("Logging out")
+        logout_button = self.driver.find_element(by=By.ID, value='Logout')
+        logout_button.click()
+
+        print("Redirecting to productapp page")
+        message = self.driver.find_element(by=By.ID, value='message0').text
+        expected_message = "Logged out successfully"
+        self.driver.save_screenshot('./e2e Test/Screenshots/12_loggedout.png')
+
+        self.assertIn(expected_message, message)
+
+    def test_10_login_failure(self):
+        print("Visiting login page")
+        register_button = self.driver.find_element(by=By.ID, value='Login')
+        register_button.click()
+
+        print("Filling login form")
+        username_field = self.driver.find_element(by=By.NAME, value='username')
+        username_field.send_keys('wrong')
+        password_field = self.driver.find_element(by=By.NAME, value='password')
+        password_field.send_keys('wrong')
+
+        print("Submitting login form")
+        register_button = self.driver.find_element(by=By.ID, value='Submit')
+        register_button.click()
+
+        print("Redirecting to login page")
+        message = self.driver.find_element(by=By.ID, value='error').text
+        expected_message = "User not registered in database"
+        self.driver.save_screenshot('./e2e Test/Screenshots/13_loginfailed.png')
+
         self.assertIn(expected_message, message)
 
     @classmethod
     def tearDownClass(inst):
+        inst.end = time.time()
+        elapsedtime=inst.end-inst.start
+        print("\n-------\nE2E test duration: ", "{:.2f}".format(elapsedtime), "seconds")
         inst.driver.quit()
+        deleteUser()
 
 
 
-# driver = webdriver.Chrome('../chromedriver.exe')
-#
-# print("Visiting home page")
-# driver.get('http://localhost:5000/')
-# driver.maximize_window()
-# driver.save_screenshot('./Screenshots/homepage.png')
-# #time.sleep(2)
-#
-#
-# driver.quit()
+
